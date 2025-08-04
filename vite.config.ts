5234dec65789c04af9effe179ac6b3a0d79f5ed7
César Antonio Pereira Flores <cesar.pereira@unitec.edu>
2025-08-04 15:50:17 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // <-- Cambia esto
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    allowedHosts: ["all"],
-  },
-})
+});
